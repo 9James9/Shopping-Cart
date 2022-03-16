bd1a2fa33a1c98381f3c1d65d495993abf66f5ed
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-16 13:52:43 -0500

    started adding quantity to items

--- src/Components/Shop/Item/item.js
+++ src/Components/Shop/Item/item.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react'
 const Item = ({name,id, updateShop}) => {
     let values = {
         name: name,
-        id: id
+        id: id,
+        quantity: 1
     }
   return (
     <div>
