b45957100680149663a0c57d902b776fe0f125a8
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-17 15:00:21 -0500

    added css to header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -4,14 +4,17 @@ import './header.css'
 const Header = () => {
     const navigate = useNavigate()
   return (
-    <div>
-        <div>
-            <ul>
+    <div className='header__container'>
+        <div className='header_nav--container'>
+            <ul className='header__navlist'>
                 <li className="header__listItem header__listItem--home"onClick={() => navigate("/")}>Home</li>
                 <li className="header__listItem header__listItem--shop" onClick={() => navigate("/shop")}>Shop</li>
                 <li className="header__listItem header__listItem--cart" onClick={() => navigate("/cart")}>Checkout</li>
                 <li className="header__listItem header__listItem--contact" onClick={() => navigate("/bothlol")}>Contact</li>
             </ul>
+        </div>
+        <div className='header__title--container'>
+            <h1 className='header__title'>Zamazonia</h1>
         </div>
     </div>
   )
