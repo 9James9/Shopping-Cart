500f1df743e8f33a6864e6bbb7a3632b58e601db
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-17 14:40:56 -0500

    added class names to header list items

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -7,10 +7,10 @@ const Header = () => {
     <div>
         <div>
             <ul>
-                <li onClick={() => navigate("/")}>Home</li>
-                <li onClick={() => navigate("/shop")}>Shop</li>
-                <li onClick={() => navigate("/cart")}>Checkout</li>
-                <li onClick={() => navigate("/bothlol")}>Contact</li>
+                <li className="header__listItem header__listItem--home"onClick={() => navigate("/")}>Home</li>
+                <li className="header__listItem header__listItem--shop" onClick={() => navigate("/shop")}>Shop</li>
+                <li className="header__listItem header__listItem--cart" onClick={() => navigate("/cart")}>Checkout</li>
+                <li className="header__listItem header__listItem--contact" onClick={() => navigate("/bothlol")}>Contact</li>
             </ul>
         </div>
     </div>
