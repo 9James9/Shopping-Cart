d39ac3f65a461bbc7912c29841e88cc1c8984006
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-17 14:37:00 -0500

    added navigate to header list items

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './header.css'
 const Header = () => {
+    const navigate = useNavigate()
   return (
-    <div>Header</div>
+    <div>
+        <div>
+            <ul>
+                <li onClick={() => navigate("/")}>Home</li>
+                <li onClick={() => navigate("/shop")}>Shop</li>
+                <li onClick={() => navigate("/cart")}>Checkout</li>
+                <li onClick={() => navigate("/bothlol")}>Contact</li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
