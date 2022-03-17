c01ba791eeb3b8d24807751ab5bc395431c99937
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-17 13:32:25 -0500

    added and linked shop.css

--- src/Components/Shop/shop.js
+++ src/Components/Shop/shop.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Item from './Item/item'
+import './shop.css'
 const Shop = ({updateCart, removeFromCart,cart}) => {
   const [names, setNames] = useState([])
     const navigate = useNavigate()
