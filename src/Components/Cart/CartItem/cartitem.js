3642bcca7fc44ace31ea107c4afcf56a4804a2ec
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-17 19:44:54 -0500

    css for header

--- src/Components/Cart/CartItem/cartitem.js
+++ src/Components/Cart/CartItem/cartitem.js
@@ -3,10 +3,11 @@ import './cartitem.css'
 const CartItem = ({itemName, index, removeFromCart}) => {
   return (
     <div>
-        <div>
+        <div className='cartItem__container'>
             <p>{itemName}</p>
             <p>{index}</p>
             <button value={index} onClick={removeFromCart}>Remove</button>
+            <button className='quantity__button'>Increase Quantity</button>
         </div>
     </div>
   )
